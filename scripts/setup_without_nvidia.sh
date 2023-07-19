80052bbf10a2a63c02fd4d1c41e751753e4e74a3
Ivan Pavlovic <pavlovic.ivan.26@gmail.com>
2023-07-19 16:05:18 +0200

    Remove duplicated line

--- scripts/setup_without_nvidia.sh
+++ scripts/setup_without_nvidia.sh
@@ -15,7 +15,6 @@ chown -R runner:runner ~runner/runner
 
 echo "[INFO] > Installing packages"
 curl --silent -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-curl --silent -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] \
   https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" \
   | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
