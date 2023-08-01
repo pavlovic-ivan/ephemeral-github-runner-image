5270aa9e882f271e663ecc419c3ed11d505a938f
Ivan Pavlovic <pavlovic.ivan.26@gmail.com>
2023-08-01 13:44:13 +0200

    build ami for parquetsharp

--- scripts/setup_without_nvidia.sh
+++ scripts/setup_without_nvidia.sh
@@ -12,3 +12,18 @@ mkdir ~runner/runner
 echo "[INFO] > Installing GitHub Actions runner"
 curl -fsSL https://github.com/actions/runner/releases/download/v${RUNNER_VERSION}/actions-runner-linux-${ARCH}-${RUNNER_VERSION}.tar.gz | tar -C ~runner/runner -xzf -
 chown -R runner:runner ~runner/runner
+
+echo "[INFO] > Installing packages"
+sudo apt update
+sudo apt install libicu66 -y
+curl -L -o /tmp/powershell.tar.gz https://github.com/PowerShell/PowerShell/releases/download/v7.3.6/powershell-7.3.6-linux-arm64.tar.gz
+sudo mkdir -p /opt/microsoft/powershell/7
+sudo tar zxf /tmp/powershell.tar.gz -C /opt/microsoft/powershell/7
+sudo chmod +x /opt/microsoft/powershell/7/pwsh
+sudo ln -s /opt/microsoft/powershell/7/pwsh /usr/bin/pwsh
+
+sudo chown -R runner:runner /usr/share
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh ./get-docker.sh
+sudo chmod 666 /var/run/docker.sock
+sudo systemctl start docker
