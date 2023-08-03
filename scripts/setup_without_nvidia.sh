619ff1590cab866818e5d0d11c04ad1cb61ff1ac
Ivan Pavlovic <pavlovic.ivan.26@gmail.com>
2023-08-03 13:48:23 +0200

    Install packages as runner

--- scripts/setup_without_nvidia.sh
+++ scripts/setup_without_nvidia.sh
@@ -14,16 +14,13 @@ curl -fsSL https://github.com/actions/runner/releases/download/v${RUNNER_VERSION
 chown -R runner:runner ~runner/runner
 
 echo "[INFO] > Installing packages"
-sudo apt update
-sudo apt install libicu66 -y
+sudo -su runner
 curl -L -o /tmp/powershell.tar.gz https://github.com/PowerShell/PowerShell/releases/download/v7.3.6/powershell-7.3.6-linux-arm64.tar.gz
 sudo mkdir -p /opt/microsoft/powershell/7
 sudo tar zxf /tmp/powershell.tar.gz -C /opt/microsoft/powershell/7
 sudo chmod +x /opt/microsoft/powershell/7/pwsh
 sudo ln -s /opt/microsoft/powershell/7/pwsh /usr/bin/pwsh
 
-sudo chown -R runner:runner /usr/bin/pwsh
-sudo chown -R runner:runner /opt/microsoft/powershell/7/pwsh
 sudo chown -R runner:runner /usr/share
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh ./get-docker.sh
