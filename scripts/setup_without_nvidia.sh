6b73c56376ece866ff2a558a36c3d58c108b6489
Ivan Pavlovic <pavlovic.ivan.26@gmail.com>
2023-08-03 13:55:06 +0200

    Install packages as runner

--- scripts/setup_without_nvidia.sh
+++ scripts/setup_without_nvidia.sh
@@ -14,7 +14,7 @@ curl -fsSL https://github.com/actions/runner/releases/download/v${RUNNER_VERSION
 chown -R runner:runner ~runner/runner
 
 echo "[INFO] > Installing packages"
-sudo -su runner
+sudo -su runner && cd
 curl -L -o /tmp/powershell.tar.gz https://github.com/PowerShell/PowerShell/releases/download/v7.3.6/powershell-7.3.6-linux-arm64.tar.gz
 sudo mkdir -p /opt/microsoft/powershell/7
 sudo tar zxf /tmp/powershell.tar.gz -C /opt/microsoft/powershell/7
