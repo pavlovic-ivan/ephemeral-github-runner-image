4f00d669250a3c1775075d5485bc77ec07caf7e9
Jonathan Giannuzzi <jgiannuzzi@users.noreply.github.com>
2023-09-01 15:48:14 +0100

    Install sudo in all images

--- scripts/setup_without_nvidia.sh
+++ scripts/setup_without_nvidia.sh
@@ -5,9 +5,14 @@ export DEBIAN_FRONTEND=noninteractive
 echo "[INFO] > Disable unattended upgrades services"
 systemctl disable --now unattended-upgrades.service
 
+echo "[INFO] > Install base packages"
+apt-get update
+apt-get install -y sudo
+
 echo "[INFO] > Creating user runner with home directory"
 /usr/sbin/useradd -m runner
 mkdir ~runner/runner
+echo "runner ALL=(ALL:ALL) NOPASSWD: ALL" > /etc/sudoers.d/runner
 
 echo "[INFO] > Installing GitHub Actions runner"
 curl -fsSL https://github.com/actions/runner/releases/download/v${RUNNER_VERSION}/actions-runner-linux-${ARCH}-${RUNNER_VERSION}.tar.gz | tar -C ~runner/runner -xzf -
